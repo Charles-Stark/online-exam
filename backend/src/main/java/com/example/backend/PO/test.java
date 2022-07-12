47be9c7cb336871b39ca4535ce5646dd0984d13f
charles_stark <1614869409@qq.com>
2022-07-12 17:08:54 +0800

    update PO

---
+++ backend/src/main/java/com/example/backend/PO/test.java
@@ -0,0 +1,21 @@
+package com.example.backend.PO;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@EnableConfigurationProperties
+@TableName(value = "objective_question1")
+@Component
+public class test {
+    private int id;
+    private long paperid;
+    private String course;
+    private String madedate;
+    private int limitedtime;
+    private long teacherid;
+    private long testid;
+    private long mader;
+}
