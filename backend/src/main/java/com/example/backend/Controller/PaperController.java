2f674d12026aa3dc0d9d697ef562c31bbccadcad
charles_stark <1614869409@qq.com>
2022-07-13 18:15:46 +0800

    Update API

--- backend/src/main/java/com/example/backend/Controller/PaperController.java
+++ backend/src/main/java/com/example/backend/Controller/PaperController.java
@@ -1,5 +1,7 @@
 package com.example.backend.Controller;
 
+import com.example.backend.mapper.PaperMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/paper")
 public class PaperController {
 
+    @Autowired
+    private PaperMapper paperMapper;
 }
--- backend/src/main/java/com/example/backend/Controller/UserController.java
+++ backend/src/main/java/com/example/backend/Controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.backend.Controller;
 
+import com.example.backend.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,5 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
+
+    @Autowired
+    private UserMapper userMapper;
 
 }
