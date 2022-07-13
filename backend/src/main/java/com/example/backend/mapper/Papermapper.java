e8c5a2f390106ee0ea34cb068c15ec5e8154f0f6
WUzzzqqq <109078379+WUzzzqqq@users.noreply.github.com>
2022-07-13 14:02:46 +0800

    Add files via upload

---
+++ backend/src/main/java/com/example/backend/mapper/Papermapper.java
@@ -0,0 +1,12 @@
+package com.mt.mapper;
+
+
+import com.mt.domain.Objective_question1;
+import com.mt.domain.Paper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface Papermapper {
+    public Paper selectPaperByPaperId(Long id);
+    public void insertPaper(Integer id,Long paperid,String testlevel,String testcourse,Long questionteacher,String question,String questiontype,String questionanswer);
+}
