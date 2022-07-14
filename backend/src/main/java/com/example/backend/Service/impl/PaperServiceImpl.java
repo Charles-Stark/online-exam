c6ec3b28f39388c009d7851e80bb3e99247fdcee
charles_stark <1614869409@qq.com>
2022-07-14 11:45:23 +0800

    Add PaperService

--- backend/src/main/java/com/example/backend/Service/impl/PaperServiceImpl.java
+++ backend/src/main/java/com/example/backend/Service/impl/PaperServiceImpl.java
@@ -1,6 +1,41 @@
 package com.example.backend.Service.impl;
 
+import com.example.backend.PO.Paper;
 import com.example.backend.Service.PaperService;
+import com.example.backend.mapper.PaperMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
 
 public class PaperServiceImpl implements PaperService {
+
+    @Autowired
+    PaperMapper paperMapper;
+
+
+    @Override
+    public List<Paper> getAllPaper(Map map) {
+        return null;
+    }
+
+    @Override
+    public Paper getPaperDetail(String paperid) {
+        return null;
+    }
+
+    @Override
+    public void updatePaper(Paper paper) {
+
+    }
+
+    @Override
+    public Paper createPaper(Paper paper) {
+        return null;
+    }
+
+    @Override
+    public void deletePaper(String paperid) {
+
+    }
 }
