a57c8256c0b7775c5c5ae7078064e695ff61c6f7
charles_stark <1614869409@qq.com>
2022-07-14 11:39:56 +0800

    Add PaperService

--- backend/src/main/java/com/example/backend/Controller/UserController.java
+++ backend/src/main/java/com/example/backend/Controller/UserController.java
@@ -9,7 +9,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/user")
 public class UserController {
 
-    @Autowired
-    private UserMapper userMapper;
-
 }
---
+++ backend/src/main/java/com/example/backend/Service/impl/PaperServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.backend.Service.impl;
+
+import com.example.backend.Service.PaperService;
+
+public class PaperServiceImpl implements PaperService {
+}
